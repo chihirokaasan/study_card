d1b832c4006f45fb5e64902ea68349a70ef908f5
matsuda chihiro <seven.mazda@gmail.com>
2019-12-02 13:59:21 +0900

    study card 最初のコミット

---
+++ src/components/Form.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-if="errors.length">
+      <p v-for="error in errors">{{ error }}</p>
+    </div>
+    <form class="simple-form" action="" method="post" v-on:submit.prevent="onSubmit">
+      <p>りんご</p>
+      <input v-model="message" placeholder="edit me">
+      <p>write spell: {{ message }}</p>
+      <button type="submit">submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data () {
+      return {
+          message: '',
+          errors: [],
+      }
+  },
+  methods: {
+      onSubmit:function (e) {
+          this.errors = [];
+          if (!this.message) {
+              this.errors.push("ちゃんと入力しろし");
+          } else {
+              if(this.message == 'apple') {
+                  this.errors.push("正解");
+              } else {
+                  this.errors.push("不正解");
+                  this.errors.push("apple");
+              }
+
+          }
+          e.preventDefault();
+      }
+  }
+}
+</script>
